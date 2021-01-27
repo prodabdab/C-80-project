cd8dabc941d38f7b4ad54f68cff8386542a1f48f
prodabdab <73546028+prodabdab@users.noreply.github.com>
2021-01-27 17:22:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var name_array=[];
+
+function First_P() {
+
+    var array_name11=document.getElementById("name_1").value;
+    var array_name22=document.getElementById("name_2").value;
+    var array_name33=document.getElementById("name_3").value;
+    var array_name44=document.getElementById("name_4").value;
+
+
+    name_array1.push(array_name11);
+    name_array1.push(array_name22);
+    name_array1.push(array_name33);
+    name_array1.push(array_name44);
+
+    document.getElementById("outputp1").innerHTML=name_array1;
+
+}
+
+function second_P() {
+
+    var array_name1=document.getElementById("1name_1").value;
+    var array_name2=document.getElementById("1name_2").value;
+    var array_name3=document.getElementById("1name_3").value;
+    var array_name4=document.getElementById("1name_4").value;
+
+    name_array1.push(array_name1);
+    name_array1.push(array_name2);
+    name_array1.push(array_name3);
+    name_array1.push(array_name4);
+
+    document.getElementById("output").innerHTML=name_array;
+}
+
